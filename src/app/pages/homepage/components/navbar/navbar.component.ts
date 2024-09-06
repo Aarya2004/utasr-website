a98cf93614b2a19a83bde8570d9ca17a327a3ff2
dannyb3334 <117242411+dannyb3334@users.noreply.github.com>
2024-09-06 13:36:21 -0400

    Fixed Navbar scroll points

--- src/app/pages/homepage/components/navbar/navbar.component.ts
+++ src/app/pages/homepage/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { MenubarModule } from 'primeng/menubar';
 
@@ -7,29 +7,34 @@ import { MenubarModule } from 'primeng/menubar';
   standalone: true,
   imports: [MenubarModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  @Output() navItemClicked = new EventEmitter<string>();
   navitems: MenuItem[] | undefined;
 
-    ngOnInit() {
-        this.navitems = [
-          {
-              label: 'Home',
-              style: {'margin-left': 'auto'}
-          },
-          {
-              label: 'About Us'
-          },
-          {
-              label: 'Rhino'
-          },
-          {
-              label: 'Join'
-          },
-          {
-              label: 'Contact'
-          }
-        ]
-    }
+  ngOnInit() {
+    this.navitems = [
+      {
+        label: 'Home',
+        command: () => this.navItemClicked.emit('home')
+      },
+      {
+        label: 'About Us',
+        command: () => this.navItemClicked.emit('about')
+      },
+      {
+        label: 'Rhino',
+        command: () => this.navItemClicked.emit('rhino')
+      },
+      {
+        label: 'Join',
+        command: () => this.navItemClicked.emit('join')
+      },
+      {
+        label: 'Contact',
+        command: () => this.navItemClicked.emit('contact')
+      }
+    ]
+  }
 }
